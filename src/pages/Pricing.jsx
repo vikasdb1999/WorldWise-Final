fb061a3efecb310af9923c0ee6b00ccf57e93d44
vikasdb1999 <76481213+vikasdb1999@users.noreply.github.com>
2023-10-06 01:02:15 +0530

    Update Pricing.jsx

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -13,11 +13,7 @@ export default function Product() {
             <br />
             Just ₹100/month.
           </h2>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel
-            labore mollitia iusto. Recusandae quos provident, laboriosam fugit
-            voluptatem iste.
-          </p>
+          
         </div>
         <img src="img-2.jpg" alt="overview of a large city with skyscrapers" />
       </section>
